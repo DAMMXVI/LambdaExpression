193c7147e2bcdf4a56b8f9ebedaed74c86919312
Alper Deniz <48470323+DAMMXVI@users.noreply.github.com>
2020-01-31 14:54:17 +0300

    create lambda expression with manually.

--- lambda.cs
+++ lambda.cs
@@ -10,10 +10,24 @@
             // bir değer alıp bool dönen foksiyonlara genel olarak Predicate denir. Expression'ı derleyerek bir fonksiyon elde ediyoruz.
             Func<People, bool> predicate = lambda.Compile();
             // bir people ile test edelim
-            
+
+
             var result = predicate(people);
             Console.WriteLine(result);  //True!
+
+            // parametremizi tanımlayalım, diğer Expression'lar bu parametreye isimle değil, bu değişken ile ulaşmak zorundalar
+            var prm = Expression.Parameter(typeof(People));
+            var lambda2 = Expression.Lambda(
+                Expression.MakeBinary(                      // Lambda Body özelliği bir BinaryExpression
+                    ExpressionType.GreaterThan,             // Büyüktür karşılaştırması yapacağız
+                    Expression.PropertyOrField(prm, "Id"),  // Dışarıdan gelen parametrenin Id özelliği
+                    Expression.Constant(1)                  // sabit değerimizi ConstantExpression ile geçiyoruz
+                ),
+                prm     // Lambda'nın bu parametreyi dışarıdan beklediğini belirtiyoruz
+            );
+            //Böylece runtime'da bir lambda expression oluşturduk.
             
+            Console.ReadKey();
         }
     }
 
